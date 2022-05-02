dd943640f080a37e61cd38bf4af5026d47e31d2e
nktinn <103056035+nktinn@users.noreply.github.com>
2022-05-03 00:27:49 +0300

    Update LoginModel.cs

--- Models/LoginModel.cs
+++ Models/LoginModel.cs
@@ -3,12 +3,12 @@
     public class LoginModel
     {
         [Required(ErrorMessage = "Не указан электронный адрес")]
-        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Некорректный адрес")]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Указан некорректный адрес")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Не указан пароль")]
         [DataType(DataType.Password)]
-        [StringLength(32, MinimumLength = 8, ErrorMessage = "Длина строки должна быть от {2} до {1} символов")]
+        [StringLength(32, MinimumLength = 8, ErrorMessage = "Пароль должен быть от {2} до {1} символов")]
         public string Password { get; set; }
 
     }
