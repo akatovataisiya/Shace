b2cec443d0f11e215f2ffe03c84f9cae13d49175
nktinn <103056035+nktinn@users.noreply.github.com>
2022-05-04 23:28:53 +0300

    Update RegisterModel.cs

--- Models/RegisterModel.cs
+++ Models/RegisterModel.cs
@@ -2,23 +2,23 @@
 {
     public class RegisterModel
     {
-        [Required(ErrorMessage = "Не указан электронный адрес")]
-        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Указан некорректный адрес")]
+        [Required(ErrorMessage = "• Не указан электронный адрес")]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "• Указан некорректный адрес")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Не указан никнейм")]
-        [StringLength(20, MinimumLength = 6, ErrorMessage = "Никнейм должен быть от {2} до {1} символов")]
+        [Required(ErrorMessage = "• Не указан никнейм")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "• Никнейм должен быть от {2} до {1} символов")]
         public string ShortName { get; set; }
 
-        [Required(ErrorMessage = "Не указан пароль")]
+        [Required(ErrorMessage = "• Не указан пароль")]
         [DataType(DataType.Password)]
-        [StringLength(32, MinimumLength = 8, ErrorMessage = "Пароль должен быть от {2} до {1} символов")]
+        [StringLength(32, MinimumLength = 8, ErrorMessage = "• Пароль должен быть от {2} до {1} символов")]
         public string Password { get; set; }
 
-        [Required(ErrorMessage = "Не указан подтверждающий пароль")]
-        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        [Required(ErrorMessage = "• Не указан подтверждающий пароль")]
+        [Compare("Password", ErrorMessage = "• Пароли не совпадают")]
         [DataType(DataType.Password)]
-        public virtual string ConfirmPassword { get; set; }
+        public string ConfirmPassword { get; set; }
 
     }
 }
