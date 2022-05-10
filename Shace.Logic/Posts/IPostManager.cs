3706325c72f30361a32b1161d5ec87dad4b40ab2
nktinn <103056035+nktinn@users.noreply.github.com>
2022-05-11 00:11:59 +0300

    Posts

---
+++ Shace.Logic/Posts/IPostManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shace.Logic.Posts
+{
+    internal interface IPostManager
+    {
+        public Task<bool> AddPost();
+        public Task<bool> UpdatePost();
+        public Task<bool> DeletePost();
+        public Task<bool> DeleteAllPost();
+        public Post GetPost();
+    }
+}
---
+++ Shace.Logic/Posts/PostManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shace.Logic.Posts
+{
+    public class PostManager : IPostManager
+    {
+        private readonly AccountContext _context;
+
+        public PostManager(AccountContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> AddPost()
+        {
+            /*var accountInDb = _context.Accounts.FirstOrDefault(acc => (acc.Email == email || acc.ShortName == shortName));
+            if (accountInDb == null)
+            {
+                var account = new Account();
+                account.Email = email;
+                account.ShortName = shortName;
+                account.Password = password;
+                account.RegDay = DateTime.Now;
+                account.Url = $"https://shace.com/{shortName}";
+                account.Privacy = false;
+                _context.Accounts.Add(account);
+                await _context.SaveChangesAsync();*/
+                return true;
+        }
+
+        public Task<bool> DeleteAllPost()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeletePost()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Post GetPost(int id)
+        {
+            var postInDb = _context.Posts.FirstOrDefault(post => post.AccountId == id);
+            if (postInDb != null)
+                return postInDb;
+            else return null;
+        }
+
+        public Post GetPost()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdatePost()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
