ab50b057e9483181f58a16ea000efa6de66250a5
UniQlow17 <103075894+UniQlow17@users.noreply.github.com>
2022-05-22 16:32:38 +0300

    Chat Update

--- Shace.Logic/ProfilePage/ProfilePageManager.cs
+++ Shace.Logic/ProfilePage/ProfilePageManager.cs
@@ -58,5 +58,61 @@
             _context.Advertisments.Add(_ad);
             _context.SaveChanges();
         }
+
+        public Dialog IfDial(Account accountInDb, Account accountInDbUser)
+        {
+            var oneAccInDbDial = _context.Dialogs.FirstOrDefault(s => (s.AccountId == accountInDb.Id && s.Account2Id == accountInDbUser.Id || s.Account2Id == accountInDb.Id && s.AccountId == accountInDbUser.Id));
+            if (oneAccInDbDial != null)
+                return oneAccInDbDial;
+            else return null;
+        }
+
+        public int Dialog(Account accountInDb, Account accountInDbUser)
+        {
+            var dial = IfDial(accountInDb, accountInDbUser);
+            if (dial == null)
+            {
+                var dialog = new Dialog();
+                dialog.AccountId = accountInDb.Id;
+                dialog.Account2Id = accountInDbUser.Id;
+                _context.Dialogs.Add(dialog);
+                _context.SaveChanges();
+                return dialog.Id;
+            }
+            return dial.Id;
+        }
+
+        public List<Account> GetDial(Account accountInDb)
+        {
+            var dialogs = _context.Dialogs.Where(d => (d.AccountId == accountInDb.Id || d.Account2Id == accountInDb.Id)).ToList();
+            var users = new List<Account>();
+            foreach (var dial in dialogs)
+            {
+                users.Add(_context.Accounts.FirstOrDefault(a => (a.Id != accountInDb.Id && (a.Id == dial.AccountId || a.Id == dial.Account2Id))));
+            }
+            return users;
+        }
+
+        public List<Message> GetMes(Account accountInDb, Account accountInDbUser, int dialId)
+        {
+            var mesacc1 = _context.Messages.Where(m => (m.DialogId == dialId && m.AccountId == accountInDb.Id));
+            var mesacc2 = _context.Messages.Where(m => (m.DialogId == dialId && m.AccountId == accountInDbUser.Id));
+            mesacc1.ToList();
+            mesacc2.ToList();
+            var messages = mesacc1.Concat(mesacc2).ToList();
+            messages = messages.OrderByDescending(m => m.MessageDate).ToList();
+            return messages;
+        }
+
+        public void SendMes(Account accountInDb, int dialId, string text)
+        {
+            var message = new Message();
+            message.DialogId = dialId;
+            message.MessageDate = DateTime.Now;
+            message.Text = text;
+            message.AccountId = accountInDb.Id;
+            _context.Messages.Add(message);
+            _context.SaveChanges();
+        }
     }
 }
